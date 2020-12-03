a502df36cd69c780210afc8b6ec42994cc8d636f
Egrassa <bacek90@mail.ru>
2020-12-03 18:57:50 +0500

    Добавлены докстринги к модулю search_result_page

--- booking/search_result_page.py
+++ booking/search_result_page.py
@@ -8,15 +8,32 @@ from booking.locators import SearchResultPageLocators
 
 
 class SearchResultPage(BasePage):
+    """
+    Реализации паттерна PageObject.
+    Класс представляет собой страницу с результатами поиска
+
+    :atribute: filters - фильтры которые доступны
+    :atribute: hotels - отели которые попадают под критерии фильтров
+    """
     filters: List[WebElement] = None
     hotels: List[WebElement] = None
 
-    def select_filters(self, needed_filters: List[str]):
+    def select_filters(self, needed_filters: List[str]) -> None:
+        """
+        На странице выбирает фильтры
+        :param needed_filters: фильтры которые надо найти и включить
+        :return: None
+        """
         for filter in needed_filters:
             self.select_filter(filter)
             sleep(5)
 
-    def select_filter(self, needed_filter: str):
+    def select_filter(self, needed_filter: str) -> None:
+        """
+        На странице выбирает фильтр
+        :param needed_filter: фильтр который надо найти и включить
+        :return: None
+        """
         self._get_filters()
         for filter in self.filters:
             if needed_filter in filter.text:
@@ -26,12 +43,22 @@ class SearchResultPage(BasePage):
         print(f'Фильтр: {needed_filter} не найден')
 
     def _get_filters(self):
+        """
+        Добавляет в атрибут обьекта фильтры доступные на странице
+        """
         self.filters = self.find_elements(SearchResultPageLocators.FILTERS)
 
     def _get_hotels(self):
+        """
+        Добавляет в атрибут обьекта отели доступные на странице
+        """
         self.hotels = self.find_elements(SearchResultPageLocators.HOTELS)
 
-    def print_info_about_hotel(self, count_hotel: int = 10):
+    def print_info_about_hotel(self, count_hotel: int = 10) -> None:
+        """
+        Выводит информацию об отелях
+        :param count_hotel: кол-во отелей о которых выведется информация
+        """
         self._get_hotels()
         for hotel in self.hotels[:count_hotel]:
             print(hotel.find_element(*SearchResultPageLocators.HOTEL_NAME).text)
@@ -39,8 +66,11 @@ class SearchResultPage(BasePage):
             print(hotel.find_element(*SearchResultPageLocators.HOTEL_SCORE).text)
             print('----------------------')
 
-    def select_cities(self):
-        for position in range(5):
+    def select_cities(self) -> None:
+        """
+        Выбирает в фильтре о городах первые 20 штук
+        """
+        for position in range(20):
             cities = self.find_elements(SearchResultPageLocators.CITIES)
             try:
                 cities[position].click()
